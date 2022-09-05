d16f1c0e0a41af7ee27cb1594ea592be9f3e4555
vyasg84 <111951537+vyasg84@users.noreply.github.com>
2022-09-05 14:27:37 +0530

    Update production.sh

--- production.sh
+++ production.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
 echo "Removing existing directory"
-rm -rf docker_python_jenkins
+rm -rf Django-todo-jenkins
 echo "Clone from GitHub"
-git clone https://github.com/vyasg84/docker_python_jenkins.git
+git clone https://github.com/vyasg84/Django-todo-app.git
 echo "removing image"
-docker image rmi  vyasg84/jenkins-python-demo:1.0
+docker image rmi  vyasg84/djnago-todo-app:1.0
+docker image rmi  vyasg84/nginx-todo:1.0
 echo "Pulling new image from DockerHub"
-docker pull vyasg84/jenkins-python-demo:1.0
-echo "Removing existing container on server"
-docker rm -f docker_python_jenkins
+docker pull vyasg84/djnago-todo-app:1.0
+docker pull vyasg84/nginx-todo:1.0
+cd Django-todo-jenkins
 echo "Creating new container on server"
-sudo docker run -d --name docker_python_jenkins vyasg84/jenkins-python-demo:1.0
+docker-compose up
 echo "Show Running container on server"
 docker ps -a
